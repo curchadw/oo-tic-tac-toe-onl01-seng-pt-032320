1728389db9773e2b64deb0ac196d585f3ee1f1fe
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-04 00:24:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -106,17 +106,7 @@ end
     end
     
     def winner
-     index = []
-     index = won?
-    if index == false
-     return nil
-    else
-     if @board[index[0]] == "X"
-      return "X"
-     else
-      return "O"
-     end
-    end
+     
     end
     
     def play
