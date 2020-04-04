d808a7f80ee786ccecfc2843e8cf24ef296c99a4
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-04 00:14:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -113,6 +113,17 @@ end
       end
     end
     
-    
+    def play
+  until over? == true
+    turn
+  end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+
   
 end
